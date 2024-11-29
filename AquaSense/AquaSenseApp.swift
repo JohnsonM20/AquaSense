c67762539d121a23012437ccedb74bdb804e7e2e
Matthew Johnson <johnsonmh36@gmail.com>
2024-11-29 21:50:04 +0100

    Initial Commit

---
+++ AquaSense/AquaSenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AquaSenseApp.swift
+//  AquaSense
+//
+//  Created by Matthew Johnson on 11/29/24.
+//
+
+import SwiftUI
+
+@main
+struct AquaSenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AquaSenseTests/AquaSenseTests.swift
@@ -0,0 +1,16 @@
+//
+//  AquaSenseTests.swift
+//  AquaSenseTests
+//
+//  Created by Matthew Johnson on 11/29/24.
+//
+
+import Testing
+
+struct AquaSenseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
